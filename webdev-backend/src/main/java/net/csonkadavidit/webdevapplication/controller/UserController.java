94dbbffab359ec74bce5788438b428507679a50a
CsonkaDavid <shay9505@gmail.com>
2022-12-13 18:32:36 +0100

    Changed responses to be more verbose

--- webdev-backend/src/main/java/net/csonkadavidit/webdevapplication/controller/UserController.java
+++ webdev-backend/src/main/java/net/csonkadavidit/webdevapplication/controller/UserController.java
@@ -21,22 +21,38 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<HttpStatus> registerUser(@RequestParam Map<String, String> userParams) {
+    public ResponseEntity<String> registerUser(@RequestParam Map<String, String> userParams) {
         Optional<UserDto> user = userService
                 .register(userParams.get("email"), userParams.get("name"), userParams.get("password"));
 
+        System.out.println("Registered user");
+
         if(user.isPresent())
-            return ResponseEntity.status(HttpStatus.CREATED).body(null);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered");
         else
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<HttpStatus> loginUser(@RequestParam String email, @RequestParam String password) {
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestParam String email, @RequestParam String password) {
         Optional<UserDto> user = userService.login(email, password);
 
+        System.out.println("Log in");
+
         if(user.isPresent())
-            return ResponseEntity.status(HttpStatus.OK).body(null);
+            return ResponseEntity.status(HttpStatus.OK).body("User logged in");
+        else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser() {
+        Optional<UserDto> user = userService.logout();
+
+        System.out.println("Log out");
+
+        if(user.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body("User logged out");
         else
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
